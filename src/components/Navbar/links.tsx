a233df1593fb8921cc473ba90697e0908ffa0438
Midhun <midhun2031@gmail.com>
2025-03-01 22:14:32 +0530

    Remove ES lint

--- src/components/Navbar/links.tsx
+++ src/components/Navbar/links.tsx
@@ -4,16 +4,16 @@ const Links = () =>{
   return (
     <div className="links flex text-sm md:text-md">
         <Link href='/'>
-        <p className="p-2 select-none cursor-pointer hover:text-[white] transition-all duration-300 text-[#b4b4b4]">Home</p>
+        <p className="p-2 select-none cursor-pointer hover:text-[white] hover:bg-[var(--secondary)] transition-all duration-300 text-[#b4b4b4]">Home</p>
         </Link>
         <Link href='/work'>
-        <p className="p-2 select-none cursor-pointer hover:text-[white] transition-all duration-300 text-[#b4b4b4]">Freelance</p>
+        <p className="p-2 select-none cursor-pointer hover:text-[white] hover:bg-[var(--secondary)]  transition-all duration-300 text-[#b4b4b4]">Freelance</p>
         </Link>
         <Link href='/about'>
-        <p className="p-2 select-none cursor-pointer hover:text-[white] transition-all duration-300 text-[#b4b4b4]">About</p>
+        <p className="p-2 select-none cursor-pointer hover:text-[white] hover:bg-[var(--secondary)]  transition-all duration-300 text-[#b4b4b4]">About</p>
         </Link>
         <Link href='/contact'>
-        <p className="p-2 select-none cursor-pointer hover:text-[white] transition-all duration-300 text-[#b4b4b4]">Contact</p>
+        <p className="p-2 select-none cursor-pointer hover:text-[white] hover:bg-[var(--secondary)]  transition-all duration-300 text-[#b4b4b4]">Contact</p>
         </Link>
        
     </div>
