50080e3cadbf8626591691975f7f34e036078a4f
Midhun Kumar <midhun2031@gmail.com>
2025-03-18 23:47:21 +0530

    Added Resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Syne } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import Navbar from '@/components/Navbar/navbar';
 import AnimatedBackground from '@/components/Navbar/animatedBackground';
@@ -24,6 +25,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${syneFont.variable}`}>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-CFFEDK5ER9"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CFFEDK5ER9');
+        `}
+        </Script>
         <SpeedInsights />
         <Navbar />
         <AnimatedBackground />
